ecb4896ad96d952b559ad3efad40bd88a0929bfb
SiddharthOturkar <siddharth29otr@gmail.com>
2023-05-14 21:22:41 +0530

    WA 9 & 10

--- school-erp/src/components/StudentsCompo.js
+++ school-erp/src/components/StudentsCompo.js
@@ -1,9 +1,9 @@
 import { Layout } from "antd";
 import { Header, Content } from "antd/es/layout/layout";
-import { Menu,Tabs,Input, Form, Button } from "antd";
+import { Menu, Tabs, Input, Form, Button, Select } from "antd";
 import DropdownCompo from "./StandardDashboard/DropdownCompo"
 
-import { DashboardOutlined, UserOutlined, UnorderedListOutlined} from '@ant-design/icons/lib/icons'
+import { DashboardOutlined, UserOutlined, UnorderedListOutlined } from '@ant-design/icons/lib/icons'
 import "./StudentsCompo.css";
 import { useNavigate } from "react-router-dom";
 
@@ -37,6 +37,7 @@ const contentStyle = {
 
 };
 
+
 function StudentsCompo() {
   const { TabPane } = Tabs;
   const handleAdd = (values) => {
@@ -45,7 +46,7 @@ function StudentsCompo() {
   };
   const navigate = useNavigate()
   return (
-    
+
     <div style={{ display: "flex", flexDirection: "row" }}>
       <Layout>
         <Sider style={siderStyle} className="siderStyle">
@@ -54,65 +55,168 @@ function StudentsCompo() {
             navigate(key)
           }}
             items={[
-              { label: "Dashboard",  key: "/FourthStandard/", icon: <DashboardOutlined /> },
-              { label: "Students",className: "students", key: "/StudentsCompo", icon: <UserOutlined /> },
+              { label: "Dashboard", key: "/FourthStandard/", icon: <DashboardOutlined /> },
+              { label: "Students", className: "students", key: "/StudentsCompo", icon: <UserOutlined /> },
               { label: "Subjects", key: "/SubjectsCompo", icon: <UnorderedListOutlined /> },
             ]}>
           </Menu>
         </Sider>
         <Layout>
           <Header style={headerStyle} className="headerStyle">
-          <DropdownCompo/>
+            <DropdownCompo />
           </Header>
           <hr></hr>
           <Content style={contentStyle}>
-          <Tabs onChange={onChange} type="card">
-        <TabPane tab="Add Students" key="add-students">
-          <Form onFinish={handleAdd}>
-            <Form.Item
-              label="Student Name"
-              name="studentName"
-              rules={[{ required: true, message: 'Please enter the student name' }]}
-            >
-              <Input size="small" className="inputcss" />
-            </Form.Item>
-            <Form.Item
-              label="Roll No"
-              name="rollNo"
-              rules={[{ required: true, message: 'Please enter the roll number' }]}
-            >
-              <Input className="inputcss"/>
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">ADD</Button>
-            </Form.Item>
-          </Form>
-        </TabPane>
+            <Tabs onChange={onChange} type="card">
+              <TabPane tab="Add Students" key="add-students" className="maindiv">
+                <Form onFinish={handleAdd}>
+                  <Form.Item
+                    label="Student Name"
+                    name="studentName"
+                    className="mainfields"
+                    rules={[{ required: true, message: 'Please enter student name and try again' }]}
+                  >
+                    <Input size="small" className="inputcss" />
+                  </Form.Item>
+                  <Form.Item
+                    label="Roll No"
+                    name="rollNo"
+                    className="mainfields"
+                    rules={[{ required: true, message: 'Please enter a valid roll no and try again' }]}
+                  >
+                    <Input className="inputcss1" />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit" className="addbtn">ADD</Button>
+                  </Form.Item>
+                </Form>
+              </TabPane>
 
-        
-        <TabPane tab="Add Marks" key="add-marks">
-          <Form onFinish={handleAdd}>
-            <Form.Item
-              label="Student Name"
-              name="studentName"
-              rules={[{ required: true, message: 'Please enter the student name' }]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Roll No"
-              name="rollNo"
-              rules={[{ required: true, message: 'Please enter the roll number' }]}
-              
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">ADD</Button>
-            </Form.Item>
-          </Form>
-        </TabPane>
-      </Tabs>
+
+              <TabPane tab="Add Marks" key="add-marks" className="maindiv">
+  <Form onFinish={handleAdd}>
+    <Form.Item
+      label="Select a Student"
+      name="studentName"
+      className="mainfields"
+      // rules={[{ required: true }]}
+    >
+      <Select
+      className="inputcss"
+        showSearch
+        style={{
+          width: 200,
+        }}
+        placeholder="Select"
+        optionFilterProp="children"
+        filterOption={(input, option) =>
+          (option?.label ?? "").includes(input)
+        }
+        filterSort={(optionA, optionB) =>
+          (optionA?.label ?? "").toLowerCase().localeCompare(
+            (optionB?.label ?? "").toLowerCase()
+          )
+        }
+        options={[
+          {
+            value: "1",
+            label: "Student A",
+          },
+          {
+            value: "2",
+            label: "Student B",
+          },
+          {
+            value: "3",
+            label: "Student C",
+          },
+          {
+            value: "4",
+            label: "Student D",
+          },
+          {
+            value: "5",
+            label: "Student E",
+          },
+          {
+            value: "6",
+            label: "Student F",
+          },
+        ]}
+      />
+    </Form.Item>
+    <Form.Item
+      label="Select a Subject"
+      name="subjectName"
+      className="mainfields"
+      // rules={[{ required: true }]}
+    >
+      <Select
+      className="inputcss"
+        showSearch
+        style={{
+          width: 200,
+        }}
+        placeholder="Select"
+        optionFilterProp="children"
+        filterOption={(input, option) =>
+          (option?.label ?? "").includes(input)
+        }
+        filterSort={(optionA, optionB) =>
+          (optionA?.label ?? "").toLowerCase().localeCompare(
+            (optionB?.label ?? "").toLowerCase()
+          )
+        }
+        options={[
+          {
+            value: "1",
+            label: "Hindi",
+          },
+          {
+            value: "2",
+            label: "English",
+          },
+          {
+            value: "3",
+            label: "Marathi",
+          },
+          {
+            value: "4",
+            label: "Maths",
+          },
+          {
+            value: "5",
+            label: "Science",
+          },
+          {
+            value: "6",
+            label: "Social Studies",
+          },
+        ]}
+      />
+    </Form.Item>
+    <Form.Item
+      label="Enter Marks"
+      name="marks"
+      className="mainfields"
+      rules={[
+        ({ getFieldValue }) => ({
+          required: getFieldValue('studentName') && getFieldValue('subjectName'),
+          message: 'Please enter marks',
+        }),
+      ]}
+      dependencies={['studentName', 'subjectName']}
+    >
+      <Input type="number" min={0} max={100} className="inputcss2" />
+    </Form.Item>
+    <Form.Item>
+      <Button type="primary" htmlType="submit" className="addbtn">
+        ADD
+      </Button>
+    </Form.Item>
+  </Form>
+</TabPane>
+            </Tabs>
           </Content>
         </Layout>
       </Layout>
