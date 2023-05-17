478777a45fbbfeae946497a77f469fc53913f20c
SiddharthOturkar <siddharth29otr@gmail.com>
2023-05-17 21:17:06 +0530

    WA 5 & 6

--- school-erp/src/components/AddsubModal.js
+++ school-erp/src/components/AddsubModal.js
@@ -16,31 +16,20 @@ function AddsubModal() {
     });
   }, []);
 
-  const handleSubmit = (e) => {
-    // e.preventDefault();
-
-    const data = {
-      title: e.target.title.value,
-      body: e.target.body.value,
-    };
-
-    axios.post("https://retoolapi.dev/FlCaNC/posts", data).then((response) => {
-      setPosts([...posts, response.data]);
-    });
+  const handleSubmit = () => {
+    // Make an API request to add the subject using Axios
+    axios.post('https://retoolapi.dev/FlCaNC/posts', { title: subjectName })
+      .then(response => {
+        // Handle successful response
+        console.log('Subject added successfully');
+        setModal2Open(false);
+      })
+      .catch(error => {
+        // Handle error
+        console.error('Error adding subject:', error);
+      });
   };
-  // const handleAddSubject = () => {
-    
-  //   const newSubject = {
-  //     id: Math.random().toString(36).substr(2, 9), // Generate a random ID for each subject
-  //     name: subjectName,
-      
-  //   };
-  //   console.log(newSubject.name);
-  //   setSubjectName(prevArray => [...prevArray,newSubject])
 
-  //   setSubjects([...subjects, newSubject]);
-  //   setSubjectName('');
-  // };
   const [modal2Open, setModal2Open] = useState(false);
     return (
         <>
@@ -49,25 +38,25 @@ function AddsubModal() {
       </Button>
      
       <Modal
-        title="Add a Subject"
-        centered
-        open={modal2Open}
-        okText="Add"
-        // onOk={() => setModal2Open(false)}
-        onOk={() => handleSubmit()}
-        onCancel={() => setModal2Open(false)}
-      >
-         <hr/>
-         <div className="subjectAdd">
-            <p>Subject:</p>
-            <Input type="text"
-            name="title"
-        value={subjectName}
-        onChange={(e) => setSubjectName(e.target.value)}
-        placeholder="Enter subject name" />
-         </div>
-
-      </Modal>
+  title="Add a Subject"
+  centered
+  open={modal2Open}
+  okText="Add"
+  onOk={handleSubmit}
+  onCancel={() => setModal2Open(false)}
+>
+  <hr/>
+  <div className="subjectAdd">
+    <p>Subject:</p>
+    <Input
+      type="text"
+      name="title"
+      value={subjectName}
+      onChange={(e) => setSubjectName(e.target.value)}
+      placeholder="Enter subject name"
+    />
+  </div>
+</Modal>
     </>
     )
 }
